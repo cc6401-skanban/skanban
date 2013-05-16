f82846a06f17795c6d3ee2a7c5eea58bdfc6f5ba
Cephei <Cephei.1313@gmail.com>
2013-05-16 15:35:22 -0400

    se agrega carpeta con codigo en python para mover una imagen + imagen de prueba

--- helloworld.py
+++ helloworld.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 import wx
 import sys
-import  cStringIO
 
 class Panel1(wx.Panel):
   """ class Panel1 creates a panel with an image on it, inherits wx.Panel """
-  def __init__(self, parent, id, imageFile):
+  def __init__(self, parent, id, jpg1):
     # create the panel
     wx.Panel.__init__(self, parent, id)
     try:
         # alternate (simpler) way to load and display a jpg image from a file
         # actually you can load .jpg  .png  .bmp  or .gif files
-        jpg1 = wx.Image(imageFile, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        #jpg1 = wx.Image(imageFile, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
         # bitmap upper left corner is in the position tuple (x, y) = (5, 5)
         wx.StaticBitmap(self, -1, jpg1, (0,0), (jpg1.GetWidth(), jpg1.GetHeight()))
     except IOError:
@@ -26,7 +25,7 @@ jpg1 = wx.Image(imageFile, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
 
 frame1 = wx.Frame(None, -1, "An image on a panel", size = (jpg1.GetWidth(), jpg1.GetHeight()))
 # call the derived class
-Panel1(frame1,-1, sys.argv[1])
+Panel1(frame1,-1, jpg1)
 frame1.Show(1)
 app.MainLoop()
 
