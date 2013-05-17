6522acd83d0abea8b68f835ad2ec2c5e26973ed6
Ivan Pliouchtchai <ivan.pli@gmail.com>
2013-05-17 11:40:26 -0400

    Estructuras sin persistencia
    Primera implementacion de las estructuras Postit y Board. Sin
    pesistencia, con los tests

--- loader/Board.py
+++ loader/Board.py
@@ -1,3 +1,18 @@
+from Postit import Postit
 
 class Board:
 	postits = []
+	title = ""
+	background = ""
+	sizeX = 800
+	sizeY = 600
+
+	def __init__(self, postits=[], title = "", background="", sizeX=800, sizeY=600):
+		self.postits = postits
+		self.title = title
+		self.background = background
+		self.sizeX = sizeX
+		self.sizeY = sizeY
+	
+	def addPostit(self, p):
+		self.postits.append(p)
--- loader/Postit.py
+++ loader/Postit.py
@@ -6,9 +6,20 @@ class Postit:
 	sizeX = 0
 	sizeY = 0
 
+	def __init__(self, path="", posX=0, posY=0, sizeX=0, sizeY=0):
+		self.path = path
+		self.posX = posX
+		self.posY = posY
+		self.sizeX = sizeX
+		self.sizeY = sizeY
+	
 	def moveTo(self, x, y):
 		self.posX = x
 		self.posY = y
 	
+	def getPosition(self):
+		l = [self.posX, self.posY]
+		return l
 	
+
 
