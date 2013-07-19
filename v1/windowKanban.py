826b328a5d50be1626817132c9c360f1b969dd1a
felipe hernández <cephei.1313@gmail.com>
2013-07-19 12:52:22 -0400

    no funciona insertar texto

--- v1/windowKanban.py
+++ v1/windowKanban.py
@@ -63,12 +63,14 @@ class windowKanban():
 
         # elementos de la lista del menu Editar
         m_addPostIt = menu.Append(wx.NewId(), "&Agregar post-it", "Agregar post-it manualmente al kanban")
+        m_addText = menu.Append(wx.NewId(), "&Agregar Texto", "Agregar texto manualmente al kanban")
         m_changeColor = menu.Append(wx.NewId(), "&Cambiar color fondo", "Cambia el color de fondo de la ventana")
         #m_addVerticalLine = menu.Append(wx.NewId(), "Agregar linea &Horizontal", "Agrega una linea vertical en la posicion donde se hace clic")
         #m_addHorizontalLine = menu.Append(wx.NewId(), "Agregar linea &Vertical", "Agrega una linea horizontal en la posicion donde se hace clic")
 
         # se asocia un metodo al evento clic del elemento del menu
         self.frame.Bind(wx.EVT_MENU, self.onAddPostIt, m_addPostIt)
+        self.frame.Bind(wx.EVT_MENU, self.onAddText, m_addText)
         self.frame.Bind(wx.EVT_MENU, self.onChangeColor, m_changeColor)
         #self.frame.Bind(wx.EVT_MENU, self.onAddVerticalLine, m_addVerticalLine)
         #self.frame.Bind(wx.EVT_MENU, self.onAddHorizontalLine, m_addHorizontalLine)
@@ -181,6 +183,60 @@ class windowKanban():
 
         #self.frame = wx.Frame(None, title='Photo Control')
         #self.frame.Show(True)
+        
+        ############
+        
+    def onAddText(self, event):
+        
+        text = "Some Text"
+        #bg_colour = wx.Colour(57, 115, 57)  # matches the bg image
+        font = wx.Font(15, wx.ROMAN, wx.NORMAL, wx.BOLD)
+        textExtent = self.frame.dc.GetFullTextExtent(text, font)
+        print textExtent[0]
+        print textExtent[1]
+        print font
+        print textExtent				
+        # create a bitmap the same size as our text
+        #np_array = np.array(textExtent[0], textExtent[1])
+
+        np_array = np.zeros((textExtent[0], textExtent[1], 1), np.uint8)
+        print np_array.size
+        #np_array = cv2.cvtColor(np_array, cv2.COLOR_BGR2GRAY)
+        
+        # 'draw' the text onto the bitmap
+        #self.frame.dc.SelectObject(bmp)
+        #self.frame.dc.SetBackground(wx.Brush(bg_colour, wx.SOLID))
+        #self.frame.dc.Clear()
+        """
+        self.frame.dc.SetTextForeground(wx.RED)
+        self.frame.dc.SetFont(font)
+        self.frame.dc.DrawText(text, 0, 0)
+        self.frame.dc.SelectObject(wx.NullBitmap)
+        mask = wx.Mask(bmp, bg_colour)
+        bmp.SetMask(mask)
+        shape = DragShape(bmp)
+        shape.pos = (5, 100)
+        shape.text = "Some dragging text"
+        self.shapes.append(shape)
+        """
+        """
+       	#img = cv2.putText(np_array, text , (12,12), cv2.FONT_HERSHEY_PLAIN, 1,  255)
+       	cv2.putText(np_array, "Watermarking with OpenCV", (5,5), cv2.FONT_HERSHEY_PLAIN, fontScale=1.0, color=(255,255,255), thickness=1)
+        # ancho y alto
+        
+        h = textExtent[0]
+        w = textExtent[1]
+        # posicion por defecto
+        x = 20
+        y = 20
+        
+        # para guardar una imagen del texto y tratarlo como un postit
+        parser = Parser()
+        path_ = parser.saveImage(self.kanban.path, len(self.kanban.postits)+1, np_array)
+        self.kanban.postits.append(Postit(path_, x, y, w, h))
+        self.kanban.save()        
+        """     
+        ###########
         
     def onChangeColor(self, event):
         if not hasattr(self, "colourData"):
